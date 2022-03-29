a3794fb8a6af39e440f89fb27e97c33d189de1b2
akemialice <akemialicetomioka@gmail.com>
2022-03-29 14:30:26 -0300

    adicionando calculadora imc

--- Program.cs
+++ Program.cs
@@ -6,9 +6,20 @@ namespace CalculadoraIMC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite seu nome:");
+            string nome = Console.ReadLine();
 
-            
+            Console.WriteLine("Digite seu peso:");
+            float peso = Convert.ToSingle(Console.ReadLine());   
+
+            Console.WriteLine("Digite sua altura:");
+            float altura = Convert.ToSingle(Console.ReadLine());    
+
+            float imc = peso /(altura*altura);
+
+            Console.WriteLine("{0}, sua altura é {1} e seu peso {2}.", nome, altura, peso);
+            Console.WriteLine("Seu IMC é {0}", imc);
+          
         }
     }
 }
